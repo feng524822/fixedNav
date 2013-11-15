c25cceea09cf118738d9c502b959e82af11fe254
shelly <822481516@qq.com>
2013-11-15 20:13:07 +0800

    update the plugin
    add the var isFixed each

--- fixedNav.js
+++ fixedNav.js
@@ -1,13 +1,13 @@
 ;(function(global, $) {
 
-	$.fn.fixedNav = function(offset, adaption) {
+	$.fn.fixedNav = function(offset) {
 		var scrollTimer,
 			tDefault = [],
 			checkList = this,
 			view  = $(global),
 			adaption = !!adaption,
 			oMarTop = offset || 0;
-
+	
 		if(checkList.length) {
 			$(window).bind('scroll', function() {
 				getDefault();
@@ -17,34 +17,36 @@
 				getDefault();
 			});
 		}
-
+	
 		function getDefault() {
 			checkList.each(function(index) {
 				var	$this = $(this),
 					arr = [];
-				arr['dWidth'] = $this.width();
 				arr["startPos"] = $this.offset().top;
 				arr["dTop"] = $this.css('top') || 'auto';
 				arr["dZindex"] = $this.css('z-index') || '1';
 				arr["dPosititon"] = $this.css('posititon') || 'static';
+				arr["isFixed"] = false;
 				tDefault.push(arr);
 			});
 		}
-
+	
 		function checkScroll() {
 			var scrollTop = view.scrollTop();
 			checkList.each(function(index) {
 				var $this = $(this),
 					widthSet = adaption ? "100%" : tDefault[index]['dWidth'];
 				if(scrollTop > tDefault[index]['startPos']+ oMarTop) {
-					$this.css({"position": "fixed", "top": "0", "width": widthSet, "z-index": "999"});
+					if(tDefault[index]['startPos']) {return true;}
+					$this.css({"position": "fixed", "top": "0","z-index": "999"});
+					tDefault[index]['startPos'] = true;
 				} else {
-					$this.css({"position": tDefault[index]['dPosititon'], "top": tDefault[index]['dTop'], "width": widthSet, "z-index": tDefault[index]['dZindex']});
+					$this.css({"position": tDefault[index]['dPosititon'], "top": tDefault[index]['dTop'], "z-index": tDefault[index]['dZindex']});
 				}
 			});
 			scrollTimer = setTimeout(arguments.callee, 6);
 		}
-
+	
 	}
 
 })(window, jQuery);
