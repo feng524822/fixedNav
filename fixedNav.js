2d9f1a38374112816121f59b9d39a3214ab54ad6
shelly <822481516@qq.com>
2013-11-17 23:34:08 +0800

    优化插件性能

--- fixedNav.js
+++ fixedNav.js
@@ -1,52 +1,53 @@
 ;(function(global, $) {
 
 	$.fn.fixedNav = function(offset) {
-		var scrollTimer,
-			tDefault = [],
-			checkList = this,
-			view  = $(global),
-			adaption = !!adaption,
-			oMarTop = offset || 0;
-	
-		if(checkList.length) {
-			$(window).bind('scroll', function() {
-				getDefault();
-				clearTimeout(scrollTimer);
-				scrollTimer = setTimeout(checkScroll, 16);
-			}).bind('resize', function() {
-				getDefault();
-			});
+			var scrollTimer,
+				tDefault = [],
+				checkList = this,
+				view  = $(global),
+				adaption = !!adaption,
+				oMarTop = offset || 0;
+
+			if(checkList.length) {
+				$(window).bind('scroll', function() {
+					getDefault();
+					clearTimeout(scrollTimer);
+					scrollTimer = setTimeout(checkScroll, 16);
+				}).bind('resize', function() {
+					getDefault();
+				});
+			}
+
+			function getDefault() {
+				checkList.each(function(index) {
+					var	$this = $(this),
+						arr = [];
+					arr["startPos"] = $this.offset().top;
+					arr["dTop"] = $this.css('top') || 'auto';
+					arr["dZindex"] = $this.css('z-index') || '1';
+					arr["dPosititon"] = $this.css('posititon') || 'static';
+					arr["isFixed"] = false;
+					tDefault.push(arr);
+				});
+			}
+
+			function checkScroll() {
+				var scrollTop = view.scrollTop();
+				checkList.each(function(index) {
+					var $this = $(this),
+						widthSet = adaption ? "100%" : tDefault[index]['dWidth'];
+					if(scrollTop > tDefault[index]['startPos']+ oMarTop) {
+						if(tDefault[index]['isFixed']) {return true;}
+						$this.css({"position": "fixed", "top": "0","z-index": "999"});
+						tDefault[index]['isFixed'] = true;
+					} else {
+						$this.css({"position": tDefault[index]['dPosititon'], "top": tDefault[index]['dTop'], "z-index": tDefault[index]['dZindex']});
+						tDefault[index]['isFixed'] = false;
+					}
+				});
+				scrollTimer = setTimeout(arguments.callee, 6);
+			}
+
 		}
-	
-		function getDefault() {
-			checkList.each(function(index) {
-				var	$this = $(this),
-					arr = [];
-				arr["startPos"] = $this.offset().top;
-				arr["dTop"] = $this.css('top') || 'auto';
-				arr["dZindex"] = $this.css('z-index') || '1';
-				arr["dPosititon"] = $this.css('posititon') || 'static';
-				arr["isFixed"] = false;
-				tDefault.push(arr);
-			});
-		}
-	
-		function checkScroll() {
-			var scrollTop = view.scrollTop();
-			checkList.each(function(index) {
-				var $this = $(this),
-					widthSet = adaption ? "100%" : tDefault[index]['dWidth'];
-				if(scrollTop > tDefault[index]['startPos']+ oMarTop) {
-					if(tDefault[index]['startPos']) {return true;}
-					$this.css({"position": "fixed", "top": "0","z-index": "999"});
-					tDefault[index]['startPos'] = true;
-				} else {
-					$this.css({"position": tDefault[index]['dPosititon'], "top": tDefault[index]['dTop'], "z-index": tDefault[index]['dZindex']});
-				}
-			});
-			scrollTimer = setTimeout(arguments.callee, 6);
-		}
-	
-	}
 
 })(window, jQuery);
